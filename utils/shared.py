4fa9b1fcf99ea1667e6a9ee996495a5ba6630c5d
Jacob <jake@jwhite.network>
2020-03-15 16:05:42 -0400

    Fixed Cyclic Import

--- utils/shared.py
+++ utils/shared.py
@@ -1,8 +1,8 @@
 """Shared functions between cogs and main program"""
-import utils
+from .datahandler import fetch
 
 
 def check_admin(ctx):
     """Checks to see if message author is in bot_admins"""
-    returned = [int(id) for id in utils.fetch("bot_admins", "id")]
+    returned = [int(id) for id in fetch("bot_admins", "id")]
     return ctx.message.author.id in returned
