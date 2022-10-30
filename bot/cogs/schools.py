c02e6f5b9a411bdbaf861173df111374f4b9b680
Jacob White <jake@jwhite.network>
2022-10-30 18:51:14 -0400

    Simplify creating schools

--- bot/cogs/schools.py
+++ bot/cogs/schools.py
@@ -82,7 +82,7 @@ class SchoolCog(commands.Cog, name="Schools"):
 
     @commands.command(name="join-school", help="Joins a schools.")
     @commands.has_role("new")
-    async def join_school(self, ctx, *, school_name: str) -> None:
+    async def join_school(self, ctx: commands.Context, *, school_name: str) -> None:
         """Join School
 
         Enables users to join a school role. school_name arguments is not to be quote separated.
@@ -129,15 +129,41 @@ class SchoolCog(commands.Cog, name="Schools"):
     @commands.check(utils.check_admin)
     async def admin_add_school(self, ctx: commands.Context, *, school_name: str) -> None:
         """
-        Admin Add school
+        Allows admins to force add a school.
+        """
+        await self.add_school(ctx, school_name, True)
 
-        Enables admins to create a school roles.
-        There is no error checking and it will not assign the newly created role
+    @commands.command(
+        name="add-school",
+        help="Adds a new school and makes a role for it.\n"
+        "Only schools on the list are allowed to join.\n"
+        "List: https://github.com/Competitive-Cyber-Clubs/School-List/blob/master/school_list.csv",
+        description="Creates a new school",
+    )
+    @commands.has_role("new")
+    async def add_school_user(
+        self, ctx: commands.Context, *, school_name: str
+    ) -> None:
+        """
+        Creates a new school role for normal user
+        """
+        await self.add_school(ctx, school_name)
+
+    async def add_school(
+        self, ctx: commands.Context, school_name: str, force: bool = False
+    ) -> None:  # pylint: disable=too-many-branches
+        """
+        Add school
+
+        Creates a new role for a school
+        A region will be automatically added based on the school_list.csv in utils.
 
         :param ctx: Command Context
         :type ctx: discord.ext.commands.Context
         :param school_name: Name of school to join
         :type school_name: str
+        :param force: Don't get conformation from user
+        :type force: bool
         :return: None
         """
         if not await utils.school_check(self.bot.school_list, school_name):
@@ -158,11 +184,32 @@ class SchoolCog(commands.Cog, name="Schools"):
         if region not in regions:
             # No region map error
             self.bot.log.error(
-                f"There is no region map for {school_name}, region: {region}, regions{regions}"
+                f"There is no region map for {school_name}, region: {region}, regions: {regions}"
             )
             return await utils.error_message(ctx, f"No region defined for {school_name}")
+        color = random.randint(0, 16777215)  # nosec
+        if not force:
+            await utils.make_embed(
+                ctx,
+                title=f"You are about to create a new school: {school_name}.",
+                description="React 👍 to this message in 60 seconds to confirm.",
+                color=color,
+            )
+            # Gives the member 60 seconds to add the reaction '👍' to the message.
+            try:
+                reactions, user = await self.bot.wait_for("reaction_add", timeout=60)
+                if not await utils.check_react(ctx, user, reactions, "👍"):
+                    raise utils.FailedReactionCheck
+            except asyncio.TimeoutError:
+                await utils.error_message(
+                    ctx,
+                    "Timed out waiting for a reaction. Please reach to the message in 30 seconds",
+                )
+                return
+            except utils.FailedReactionCheck:
+                await utils.error_message(ctx, "Wrong reaction added or added by the wrong member")
+                return
 
-        color = int(f"0x{random.randint(0, 0xFFFFFF)}", 16)  # nosec
         added_school = await ctx.guild.create_role(
             name=school_name,
             color=discord.Color(color),
@@ -188,99 +235,7 @@ class SchoolCog(commands.Cog, name="Schools"):
                 f'School "{school_name}" has been created in {region} with color of 0x{color}'
             )
             await utils.make_embed(ctx, color=color, title="Success", description=success_msg)
-
-    @commands.command(
-        name="add-school",
-        help="Adds a new school and makes a role for it.\n"
-             "Only schools on the list are allowed to join.\n"
-             "List: https://github.com/Competitive-Cyber-Clubs/School-List/blob/master/school_list.csv",
-        description="Creates a new school",
-    )
-    @commands.has_role("new")
-    async def add_school(
-            self, ctx: commands.Context, *, school_name: str
-    ) -> None:  # pylint: disable=too-many-branches
-        """
-        Add school
-
-        Enables users to create a school role. They are required to have the role "new". Schools
-        will automatically be assigned a region based on the school_list.csv in utils.
-
-        **Raises:**
-
-            utils.FailedReactionCheck: Exception is raised if the reaction check does not validate.
-
-        :param ctx: Command Context
-        :type ctx: discord.ext.commands.Context
-        :param school_name: Name of school to join
-        :type school_name: str
-        :return: None
-        """
-        if not await utils.school_check(self.bot.school_list, school_name):
-            return await utils.error_message(ctx, message="School name not valid.")
-
-        if await utils.select("schools", "school", "school", school_name):
-            self.bot.log.info(
-                f"{ctx.author.name} attempted to create a duplicate role for {school_name}"
-            )
-            return await utils.error_message(
-                ctx,
-                f"School role for {school_name} already exists.\n"
-                f"Use `?join-school {school_name}` to join it",
-            )
-
-        regions = await utils.fetch("regions", "name")
-        region = await utils.region_select(self.bot.school_list, school_name)
-        if region not in regions:
-            # No region map error
-            self.bot.log.error(
-                f"There is no region map for {school_name}, region: {region}, regions: {regions}"
-            )
-            return await utils.error_message(ctx, f"No region defined for {school_name}")
-        color = random.randint(0, 16777215)  # nosec
-        await utils.make_embed(
-            ctx,
-            title=f"You are about to create a new school: {school_name}.",
-            description="React 👍 to this message in 60 seconds to confirm.",
-            color=color,
-        )
-        # Gives the member 60 seconds to add the reaction '👍' to the message.
-        try:
-            reactions, user = await self.bot.wait_for("reaction_add", timeout=60)
-            if not await utils.check_react(ctx, user, reactions, "👍"):
-                raise utils.FailedReactionCheck
-        except asyncio.TimeoutError:
-            await utils.error_message(
-                ctx, "Timed out waiting for a reaction. Please reach to the message in 30 seconds"
-            )
-        except utils.FailedReactionCheck:
-            await utils.error_message(ctx, "Wrong reaction added or added by the wrong member")
-        else:
-            added_school = await ctx.guild.create_role(
-                name=school_name,
-                color=discord.Color(color),
-                mentionable=True,
-                hoist=False,
-                reason=f"Added by {ctx.author.name}",
-            )
-            data = [
-                school_name,
-                region,
-                color,
-                added_school.id,
-                (ctx.author.name + ctx.author.discriminator),
-                ctx.author.id,
-            ]
-            status = await utils.insert("schools", data)
-            if status == "error":
-                await utils.error_message(ctx, "There was an error with creating the role.")
-                await added_school.delete(reason="Error in creation")
-                self.bot.log.warning("Error with School Role creation.")
-            else:
-                success_msg = (
-                    f'School "{school_name}" has been created in {region} with color of 0x{color}'
-                )
-                await utils.make_embed(ctx, color=color, title="Success", description=success_msg)
+            if not force:
                 await self.join_school(ctx=ctx, school_name=school_name)
 
 
